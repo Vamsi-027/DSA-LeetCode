ee25ac71beef338a579469b5476eb2f06ba000fd
Cheruku Vamsi <65002479+Vamsi-027@users.noreply.github.com>
2025-02-17 19:55:27 -0600

    Time: 0 ms (100.00%) | Memory: 42.4 MB (27.98%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        return linearSearch(matrix, target);
+        // return linearSearch(matrix, target);
+        return binarySearch(matrix, target);
     }
 
     public boolean linearSearch(int[][] matrix, int target)
@@ -13,6 +14,38 @@ class Solution {
             {
                 if(matrix[i][j] == target) return true;
             }
+        }
+        return false;
+    }
+
+    public boolean binarySearch(int[][] matrix, int target)
+    {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        for(int i=0; i<rows; i++)
+        {
+            int leftInd = 0;
+            int rightInd = cols-1;
+            int left = matrix[i][leftInd];
+            int right = matrix[i][rightInd];
+
+            if(left <= target && right >= target)
+            {
+                while(leftInd <= rightInd)
+                {
+                    int midInd = leftInd + (rightInd-leftInd)/2;
+                    if(matrix[i][midInd] == target) return true;
+                    else if(matrix[i][midInd] < target)
+                    {
+                        leftInd = midInd+1;
+                    }
+                    else
+                    {
+                        rightInd = midInd-1;
+                    }
+                }
+            }
+            else continue;
         }
         return false;
     }
