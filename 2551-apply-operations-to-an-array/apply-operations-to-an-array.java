16cf1d690c5516b419dcc3edf6d8b99ee5865189
Cheruku Vamsi <65002479+Vamsi-027@users.noreply.github.com>
2025-02-28 20:43:41 -0600

    Time: 1 ms (96.37%) | Memory: 43.7 MB (70.32%) - LeetSync

---
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int n = nums.length;
+        for(int i=0; i<n-1; i++)
+        {
+            if(nums[i] == nums[i+1] && nums[i] != 0)
+            {
+                nums[i] *= 2;
+                nums[i+1] = 0;
+            }
+        }
+        moveZerosToEnd(nums, n);
+        return nums;
+    }
+
+    public void moveZerosToEnd(int[] nums, int n)
+    {
+        int ind = 0;
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i] != 0)
+            {
+                nums[ind++] = nums[i];
+            }
+        }
+
+        while(ind < n)
+        {
+            nums[ind++] = 0;
+        }
+    }
+}
