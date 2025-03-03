0ff35d1a0fc6c3b75cd1c605add0b5bbc7a36616
Cheruku Vamsi <65002479+Vamsi-027@users.noreply.github.com>
2025-03-03 00:52:24 -0600

    Time: 5 ms (85.39%) | Memory: 68.5 MB (10.03%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,35 +1,57 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        List<Integer> left = new ArrayList<>();
-        List<Integer> right = new ArrayList<>();
 
-        for(int i : nums)
-        {
-            if(i < pivot) left.add(i);
-            else if(i > pivot) right.add(i);
-        }
-
-        int pivotsSize = nums.length - (left.size() + right.size());
-        int[] result = new int[nums.length];
+        int n = nums.length;
+        int[] result = new int[n];
         int ind = 0;
-        for(int i : left)
+        for(int num : nums)
         {
-            result[ind] = i;
-            ind++;
-        }
-        while(pivotsSize > 0)
-        {
-            result[ind] = pivot;
-            ind++;
-            pivotsSize--;
+            if(num < pivot) result[ind++] = num;
         }
 
-        for(int i : right)
+        for(int num : nums)
         {
-            result[ind]= i;
-            ind++;
+            if(num == pivot) result[ind++] = num;
+        }
+
+        for(int num : nums)
+        {
+            if(num > pivot) result[ind++] = num;
         }
 
         return result;
+
+
+        // List<Integer> left = new ArrayList<>();
+        // List<Integer> right = new ArrayList<>();
+
+        // for(int i : nums)
+        // {
+        //     if(i < pivot) left.add(i);
+        //     else if(i > pivot) right.add(i);
+        // }
+
+        // int pivotsSize = nums.length - (left.size() + right.size());
+        // int[] result = new int[nums.length];
+        // int ind = 0;
+        // for(int i : left)
+        // {
+        //     result[ind] = i;
+        //     ind++;
+        // }
+        // while(pivotsSize > 0)
+        // {
+        //     result[ind] = pivot;
+        //     ind++;
+        //     pivotsSize--;
+        // }
+
+        // for(int i : right)
+        // {
+        //     result[ind]= i;
+        //     ind++;
+        // }
+
+        // return result;
     }
 }
