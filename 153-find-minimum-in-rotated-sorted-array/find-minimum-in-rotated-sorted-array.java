152fff406883110cb0fd8d7a68452f33e1ddfd34
Cheruku Vamsi <65002479+Vamsi-027@users.noreply.github.com>
2025-12-17 12:42:11 -0600

    Time: 0 ms (100.00%) | Memory: 43.5 MB (61.46%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,6 +1,7 @@
 class Solution {
     public int findMin(int[] nums) {
-        return bruteForceUsingLinearSearch(nums);
+        // return bruteForceUsingLinearSearch(nums);
+        return usingBinarySearch(nums);
     }
 
     public int bruteForceUsingLinearSearch(int[] nums) {
@@ -10,5 +11,28 @@ class Solution {
             if(i < minValue) minValue=i;
         }
         return minValue;
+    }
+
+    public int usingBinarySearch(int[] nums) {
+        int left = 0;
+        int right = nums.length-1;
+        int ans = Integer.MAX_VALUE;
+
+
+        while(left <= right) 
+        {
+            int mid = left + (right-left)/2;
+            if(nums[left] <= nums[mid])
+            {
+                ans = Math.min(nums[left], ans);
+                left = mid+1;
+            }
+            else 
+            {
+                ans = Math.min(ans, nums[mid]);
+                right = mid;
+            }
+        }
+        return ans;
     }
 }
