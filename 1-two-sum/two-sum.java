ea8cc3c8ee27f41da0515f259fae578a2077910c
Cheruku Vamsi <65002479+Vamsi-027@users.noreply.github.com>
2025-12-17 12:08:03 -0600

    Time: 2 ms (99.11%) | Memory: 47.3 MB (11.54%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        return bruteForce(nums, target);
+        // return bruteForce(nums, target);
+        return usingHashMap(nums, target);
     }
-
 
     public int[] bruteForce(int[] nums, int target) {
         for(int i=0; i<nums.length; i++) {
@@ -12,6 +12,19 @@ class Solution {
                 }
             }
         }
+        return new int[]{};
+    }
+
+    public int[] usingHashMap(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++)
+        {
+            int balance = target-nums[i];
+            if(map.containsKey(balance)) return new int[]{map.get(balance), i};
+            else map.put(nums[i], i);
+        }
+
         return new int[]{};
     }
 }
